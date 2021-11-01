b0bade0e3b9dc41ca4ec53470ca7e982357d8042
AthiraKG <athira.kg162@gmail.com>
2021-11-01 14:27:45 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-
-
-
-@NgModule({
-  declarations: [
-    AppComponent    
-=======
 import { WoDetailsComponent } from './wo-details/wo-details.component';
 
 import {FormsModule} from '@angular/forms';
@@ -21,16 +11,11 @@ import {ReactiveFormsModule} from '@angular/forms';
   declarations: [
     AppComponent,
     WoDetailsComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
