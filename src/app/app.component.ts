a9b573a8f9a22d86a34fea36fa178b0c72c8c654
AthiraKG <30920109+AthiraKG@users.noreply.github.com>
2021-11-01 11:09:28 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
+import { FormGroup, FormControl,ReactiveFormsModule } from '@angular/forms';
+import * as LCC from 'lightning-container';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-akg-heroku';
+export class AppComponent implements OnInit {
+  formData;
+  WONumber;
+  workType;
+  status;
+  description;
+  duration;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    LCC.addMessageHandler(function(message) {
+      console.log(message.value);
+     // document.getElementById('messageFromLC').value = message.value;
+    });
+
+    this.formData=new FormGroup({
+      WONumber:new FormControl(""),
+      WorkType:new FormControl(""),
+      Status:new FormControl(""),
+      Description:new FormControl(""),
+      Duration:new FormControl("")
+    });
+  }
+onClickSubmit(data) {  this.WONumber=data.WONumber;
+  this.workType=data.WorkType;
+  this.status=data.Status;
+  this.description=data.Description;
+  this.duration=data.Duration;
+  const myData={"WONumber":this.WONumber,
+  "WorkType":this.workType,
+  "Status":this.status,
+  "Description":this.description,
+  "Duration":this.duration
+  };
+  
+  LCC.sendMessage({
+    event: "Lccevent",
+    value:JSON.stringify(myData)
+  });
+}
 }
